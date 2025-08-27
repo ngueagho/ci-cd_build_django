7d1cd2e4ee3445e7ad86b19c27b414971a5c9b25
ngueagho <ngueaghotiodongrobertolandry@gmail.com>
2025-08-27 15:47:46 +0100

    debug = false

--- monsite/monsite/settings.py
+++ monsite/monsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-68)9j38wl9f85!013z&)y3t94!c4m_k6w*cn1d0%o0*(*mgg(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
